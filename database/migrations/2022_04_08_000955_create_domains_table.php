8d13d3db9d6cb848e0e7a97e74dffbcbf1daedcd
uswebk <usweb111@gmail.com>
2022-04-08 01:15:01 +0900

    [feature] create domains table

---
+++ database/migrations/2022_04_08_000955_create_domains_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            //
+            $table->bigIncrements('id');
+            $table->text('name')->comment('ドメイン名');
+            $table->unsignedBigInteger('user_id')->comment('ユーザーID');
+            $table->integer('price')->comment('価格');
+            $table->boolean('is_active')->comment('稼働フラグ');
+            $table->boolean('is_transferred')->comment('移管フラグ');
+            $table->boolean('is_management_only')->comment('管理のみ');
+            $table->dateTime('purchased')->comment('購入日');
+            $table->dateTime('expired_date')->comment('更新期限日');
+            $table->dateTime('canceled_at')->nullable(true)->comment('解約日');
+            $table->dateTime('deleted_at')->nullable(true)->comment('削除日');
+            $table->dateTime('updated_at')->comment('更新日');
+            $table->dateTime('created_at')->comment('登録日');
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            //
+            Schema::dropIfExists('domains');
+        });
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        <a href="{{ route('register.index') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                     @endauth
                 </div>
             @endif
