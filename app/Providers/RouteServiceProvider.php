a156e2e7d18e125f4cba199925029b4c4d4f617c
uswebk <usweb111@gmail.com>
2022-04-08 22:09:45 +0900

    [refactor] ログイン後のリダイレクト先定数名変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const DASHBOARD = '/dashboard';
 
     /**
      * The controller namespace for the application.
