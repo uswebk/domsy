69f052b1a4a9e6385bb6e78d0d8ffd4f979997ab
uswebk <usweb111@gmail.com>
2022-04-12 21:06:38 +0900

    [update] add delete modal

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .autoload({
+        jquery: ['$', 'window.jQuery']
+    })
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
