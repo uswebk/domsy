924c39685b34a33c435f16ea4b6f59c0251de49b
uswebk <usweb111@gmail.com>
2022-06-15 00:54:51 +0900

    [style] top background color

--- resources/views/frontend/top.blade.php
+++ resources/views/frontend/top.blade.php
@@ -12,18 +12,21 @@
 
 
   <style>
-
-    * {
-      background-color: #e8c46a;
-      color: #5684d5;
-    }
     html {
+      width:100%;
       height:100%;
+      color: #5684d5;
     }
     body {
       font-family: 'Nunito', sans-serif;
       margin: 0;
       height:100%;
+    }
+
+    .wrapper{
+      width:100%;
+      height: 100%;
+      background-color: #e8c46a;
     }
 
   </style>
@@ -35,17 +38,19 @@
 </head>
 
 <body>
-  <div class='d-flex flex-column align-items-center justify-content-center h-100'>
-    <div>
-      <img src='{{ asset('image/domsy.jpg') }}' alt='' class="d-block mx-auto w-75">
-    </div>
-    <div>
-      @auth
-        <a href='{{ route('dashboard.index') }}' class='text-sm text-gray-700 dark:text-gray-500'>Dashboard</a>
-      @else
-        <a href='{{ route('login') }}' class='text-sm text-gray-700 dark:text-gray-500'>Log in</a>
-        <a href='{{ route('register.index') }}' class='ml-4 text-sm text-gray-700 dark:text-gray-500'>Register</a>
-      @endauth
+  <div class='wrapper'>
+    <div class='d-flex flex-column align-items-center justify-content-center h-100'>
+      <div>
+        <img src='{{ asset('image/domsy.jpg') }}' alt='' class="d-block mx-auto w-75">
+      </div>
+      <div>
+        @auth
+          <a href='{{ route('dashboard.index') }}' class='text-sm text-gray-700 dark:text-gray-500'>Dashboard</a>
+        @else
+          <a href='{{ route('login') }}' class='text-sm text-gray-700 dark:text-gray-500'>Log in</a>
+          <a href='{{ route('register.index') }}' class='ml-4 text-sm text-gray-700 dark:text-gray-500'>Register</a>
+        @endauth
+      </div>
     </div>
   </div>
 </body>
