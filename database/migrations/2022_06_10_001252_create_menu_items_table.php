165c3e7b5f114f5427da6ff12ef26ae072476eca
uswebk <usweb111@gmail.com>
2022-06-19 17:55:07 +0900

    [refactor] menus + menu_items

--- database/migrations/2022_06_10_001252_create_menu_items_table.php
+++ database/migrations/2022_06_10_001252_create_menu_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMenusTable extends Migration
+class CreateMenuItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,22 @@ class CreateMenusTable extends Migration
      */
     public function up()
     {
-        Schema::create('menus', function (Blueprint $table) {
+        Schema::create('menu_items', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('parent_id');
             $table->string('name')->comment('メニュー名');
-            $table->boolean('is_nav')->comment('ナビゲーション有無');
+            $table->string('controller')->comment('コントローラー名');
+            $table->string('function')->comment('メソッド名');
+            $table->string('route')->comment('ルート名');
+            $table->string('description')->comment('詳細説明');
+            $table->boolean('is_screen')->comment('UI表示有無');
             $table->unsignedInteger('sort')->comment('並び順');
             $table->dateTime('updated_at')->comment('更新日');
             $table->dateTime('created_at')->comment('登録日');
 
-            $table->foreign('type_id')
+            $table->foreign('parent_id')
             ->references('id')
-            ->on('menu_types')
+            ->on('menus')
             ->cascadeOnUpdate()
             ->cascadeOnDelete();
         });
