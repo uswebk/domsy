774ca58b206a96cfac4ed5c13625ae1dcea160ee
uswebk <usweb111@gmail.com>
2022-06-25 22:36:04 +0900

    [update] use modules

--- app/Services/Application/Auth/Corporation/RegisterService.php
+++ app/Services/Application/Auth/Corporation/RegisterService.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace App\Services\Application\Auth\Corporation;
 
+use App\Constants\RoleConstant;
 use Illuminate\Support\Facades\Auth;
 
 final class RegisterService
 {
+    private $userRepository;
+
+    private $userLatestCodeRepository;
+
+    private $emailVerificationService;
+
     public function __construct(
         \App\Infrastructures\Repositories\User\UserRepositoryInterface $userRepository,
         \App\Infrastructures\Repositories\User\UserLatestCodeRepositoryInterface $userLatestCodeRepository,
         \App\Infrastructures\Mails\Services\EmailVerificationService $emailVerificationService
     ) {
+        $this->userRepository = $userRepository;
+        $this->userLatestCodeRepository = $userLatestCodeRepository;
+        $this->emailVerificationService = $emailVerificationService;
     }
 
     /**
@@ -23,10 +33,8 @@ final class RegisterService
         \App\Services\Application\InputData\AuthCorporationRegisterRequest $registerRequest
     ): void {
         // Company 登録
-
         $companyRequest = $registerRequest->getInputCompany();
         // Repository
-
 
         // ユーザー登録
         $code = $this->userLatestCodeRepository->next();
@@ -34,7 +42,7 @@ final class RegisterService
         $userRequest = $registerRequest->getInputUser();
         $user = $this->userRepository->store([
             'name' => $userRequest->name,
-            'company_id' => CompanyConstant::INDEPENDENT_COMPANY_ID,
+            'company_id' => CompanyConstant::INDEPENDENT_COMPANY_ID, // 追加したcompany_idを取得
             'role_id' => RoleConstant::DEFAULT_ROLE_ID,
             'code' => $code,
             'email' => $userRequest->email,
