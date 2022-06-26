3589ef7b5c74c9e452f597eb53bfc9583e035a4a
uswebk <usweb111@gmail.com>
2022-06-27 00:11:58 +0900

    [style] delete line

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
-
     ];
 }
