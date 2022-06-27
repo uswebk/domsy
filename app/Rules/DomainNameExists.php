f2e3935266950d949f5dbcca7c77eb722c774513
uswebk <usweb111@gmail.com>
2022-06-28 00:02:06 +0900

    [update] add DomainNameExistsRule

---
+++ app/Rules/DomainNameExists.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use App\Infrastructures\Models\Domain;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+
+final class DomainNameExists implements Rule
+{
+    /**
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        $domain = Domain::where('name', $value)->where('user_id', Auth::id())
+        ->first();
+
+        if (isset($domain)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Domain already exists';
+    }
+}
