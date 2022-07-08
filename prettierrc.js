bcb88485cb67718f474a557f061e146a2d3b2f2f
uswebk <usweb111@gmail.com>
2022-07-09 00:24:57 +0900

    [style] add Vue.js formatter

---
+++ prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    // https://prettier.io/docs/en/options.html#vue-files-script-and-style-tags-indentation
+    vueIndentScriptAndStyle: true,
+};
