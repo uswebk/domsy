4b9692f79442055c725cea444d03febc49a34ee9
uswebk <usweb111@gmail.com>
2022-08-05 22:19:55 +0900

    [update] chart test

---
+++ resources/js/components/graph/SimpleChart.vue
@@ -0,0 +1,13 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart(this.chartdata, this.options)
+  },
+}
+</script>
+
+<style></style>
--- resources/js/pages/frontend/DashboardPage.vue
+++ resources/js/pages/frontend/DashboardPage.vue
@@ -1,5 +1,33 @@
 <template>
-  <v-main> </v-main>
+  <v-main>
+    <line-chart :chartdata="chartdata" :options="options" />
+  </v-main>
 </template>
 
-<script></script>
+<script>
+import LineChart from '../../components/graph/SimpleChart.vue'
+
+export default {
+  name: 'LineChartContainer',
+  components: { LineChart },
+  data: () => ({
+    chartdata: {
+      datacollection: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20],
+          },
+        ],
+      },
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+  async mounted() {},
+}
+</script>
