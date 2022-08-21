535d7686e42cd1aa657d35a3c3e2c174a2b9eec9
uswebk <usweb111@gmail.com>
2022-08-22 00:02:12 +0900

    [refactor] add Role New,Update Dialog

---
+++ resources/js/components/role/NewDialog.vue
@@ -0,0 +1,164 @@
+<template>
+  <v-dialog v-model="open" max-width="600px">
+    <v-card>
+      <v-card-title class="pl-8">
+        <span class="text-h6">Role Create</span>
+      </v-card-title>
+      <v-card-text>
+        <v-progress-linear
+          v-if="loading"
+          color="info"
+          indeterminate
+        ></v-progress-linear>
+        <v-container v-if="!loading">
+          <v-form ref="form" lazy-validation>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  class="mt-5"
+                  label="Role Name"
+                  v-model="newRoleName"
+                  required
+                  hide-details
+                ></v-text-field>
+                <validation-error-message
+                  :message="errors.name"
+                ></validation-error-message>
+              </v-col>
+              <div class="my-10"></div>
+              <v-col cols="6" v-for="menuItem in menuItems" :key="menuItem.id">
+                <v-switch
+                  v-model="newRoles[menuItem.id]"
+                  :label="menuItem.description"
+                >
+                </v-switch>
+              </v-col>
+            </v-row>
+
+            <div class="my-5"></div>
+
+            <v-btn color="primary" @click="store">Create</v-btn>
+          </v-form>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="close"> Close </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import axios from 'axios'
+import ValidationErrorMessage from '../form/ValidationErrorMessage'
+
+export default {
+  name: 'RoleNewDialog',
+  components: {
+    ValidationErrorMessage,
+  },
+  props: {
+    isOpen: {
+      default: false,
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      loading: true,
+      roles: [],
+      newRoles: {},
+      newRoleName: '',
+      menuItems: [],
+      errors: {},
+    }
+  },
+
+  computed: {
+    open: {
+      get() {
+        return this.isOpen
+      },
+      set(value) {
+        this.errors = {}
+
+        this.$emit('close', value)
+      },
+    },
+  },
+
+  methods: {
+    resetFormData() {
+      this.newRoles = {}
+      this.newRoleName = ''
+    },
+
+    async store() {
+      try {
+        const result = await axios.post('/api/roles', {
+          name: this.newRoleName,
+          roles: this.newRoles,
+        })
+
+        this.close()
+
+        this.$emit('sendMessage', {
+          message: 'Create success',
+          status: result.status,
+        })
+      } catch (error) {
+        const status = error.response.status
+
+        if (status === 422) {
+          var responseErrors = error.response.data.errors
+          var errors = {}
+          for (var key in responseErrors) {
+            errors[key] = responseErrors[key][0]
+          }
+          this.errors = errors
+          return
+        }
+
+        let message = ''
+        if (status === 403) {
+          message = 'Illegal operation was performed.'
+          this.close()
+        }
+
+        if (status >= 500) {
+          message = 'Server Error'
+          this.close()
+        }
+
+        this.$emit('sendMessage', {
+          message: message,
+          status: status,
+        })
+      }
+
+      this.resetFormData()
+    },
+
+    close() {
+      this.open = false
+    },
+
+    async initMenuItems() {
+      const result = await axios.get('api/menus/items')
+
+      this.menuItems = result.data
+    },
+  },
+
+  async created() {
+    this.loading = true
+
+    this.initMenuItems()
+
+    this.loading = false
+  },
+}
+</script>
--- resources/js/pages/frontend/AccountPage.vue
+++ resources/js/pages/frontend/AccountPage.vue
@@ -111,103 +111,18 @@
         @sendMessage="sendMessage"
       ></delete-dialog>
 
-      <!-- Role New Dialog -->
-      <v-dialog v-model="newRoleDialog" max-width="600px">
-        <v-card>
-          <v-card-title>
-            <span class="text-h6">Role Create</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-form ref="form" lazy-validation>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field
-                      class="mt-5"
-                      label="Role Name"
-                      v-model="newRoleName"
-                      required
-                      hide-details
-                    ></v-text-field>
-                    <ValidationErrorMessage :message="storeErrors.name" />
-                  </v-col>
-                  <div class="my-10"></div>
-                  <v-col
-                    cols="6"
-                    v-for="menuItem in menuItems"
-                    :key="menuItem.id"
-                  >
-                    <v-switch
-                      v-model="newRoles[menuItem.id]"
-                      :label="menuItem.description"
-                    >
-                    </v-switch>
-                  </v-col>
-                </v-row>
+      <role-new-dialog
+        :isOpen="newRoleDialog"
+        @close="closeNewRoleModal"
+        @sendMessage="sendMessage"
+      ></role-new-dialog>
 
-                <div class="my-5"></div>
-
-                <v-btn color="primary" @click="storeRole">Create</v-btn>
-              </v-form>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeNewRoleModal">
-              Close
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      <!-- Role Update Dialog -->
-      <v-dialog v-model="editRoleDialog" max-width="600px">
-        <v-card>
-          <v-card-title>
-            <span class="text-h6">Role Update</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-form ref="form" lazy-validation>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field
-                      class="mt-5"
-                      label="Role Name"
-                      v-model="role.name"
-                      required
-                      hide-details
-                    ></v-text-field>
-                    <ValidationErrorMessage :message="updateErrors.name" />
-                  </v-col>
-                  <div class="my-10"></div>
-                  <v-col
-                    cols="6"
-                    v-for="menuItem in menuItems"
-                    :key="menuItem.id"
-                  >
-                    <v-switch
-                      v-model="role.roleItems[menuItem.id]"
-                      :label="menuItem.description"
-                    >
-                    </v-switch>
-                  </v-col>
-                </v-row>
-
-                <div class="my-5"></div>
-
-                <v-btn color="primary" @click="updateRole">Update</v-btn>
-              </v-form>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeEditRoleModal">
-              Close
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <role-update-dialog
+        :isOpen="editRoleDialog"
+        :role="role"
+        @close="closeEditRoleModal"
+        @sendMessage="sendMessage"
+      ></role-update-dialog>
 
       <!-- Delete Dialog -->
       <v-dialog v-model="deleteRoleDialog" max-width="290">
@@ -241,10 +156,10 @@ import IconHeadLine from '../../components/common/IconHeadLine'
 import GreetingMessage from '../../components/common/GreetingMessage'
 import ListTable from '../../components/account/ListTable'
 import NewDialog from '../../components/account/NewDialog'
+import RoleNewDialog from '../../components/role/NewDialog'
 import UpdateDialog from '../../components/account/UpdateDialog'
+import RoleUpdateDialog from '../../components/role/UpdateDialog'
 import DeleteDialog from '../../components/account/DeleteDialog'
-
-import ValidationErrorMessage from '../../components/form/ValidationErrorMessage'
 
 export default {
   components: {
@@ -252,9 +167,10 @@ export default {
     GreetingMessage,
     ListTable,
     NewDialog,
+    RoleNewDialog,
     UpdateDialog,
+    RoleUpdateDialog,
     DeleteDialog,
-    ValidationErrorMessage,
   },
 
   data() {
@@ -275,15 +191,10 @@ export default {
       finishInitialize: false,
       users: [],
       user: {},
-      roles: {},
+      roles: [],
       role: {
-        roleItems: {},
+        roleItems: [],
       },
-      storeErrors: {},
-      updateErrors: {},
-      menuItems: {},
-      newRoles: {},
-      newRoleName: '',
     }
   },
 
@@ -336,11 +247,6 @@ export default {
       this.deleteRoleDialog = false
     },
 
-    resetNewRoles() {
-      this.newRoles = {}
-      this.newRoleName = ''
-    },
-
     resetGreeting() {
       this.greetingType = ''
       this.message = ''
@@ -358,87 +264,6 @@ export default {
       } else {
         this.greetingType = 'error'
         this.message = result.message
-      }
-    },
-
-    async storeRole() {
-      this.resetGreeting()
-
-      try {
-        const result = await axios.post('/api/roles', {
-          name: this.newRoleName,
-          roles: this.newRoles,
-        })
-
-        if (result.status === 200) {
-          this.greeting = 'Create success'
-        }
-
-        this.initRoles()
-        this.closeNewRoleModal()
-        this.resetNewRoles()
-      } catch (error) {
-        const status = error.response.status
-
-        if (status === 403) {
-          this.alert = 'Illegal operation was performed.'
-          this.closeNewRoleModal()
-          this.resetNewRoles()
-        }
-
-        if (status === 422) {
-          var responseErrors = error.response.data.errors
-          var errors = {}
-          for (var key in responseErrors) {
-            errors[key] = responseErrors[key][0]
-          }
-          this.storeErrors = errors
-        }
-
-        if (status >= 500) {
-          this.alert = 'Server Error'
-          this.closeNewRoleModal()
-          this.resetNewRoles()
-        }
-      }
-    },
-
-    async updateRole() {
-      this.resetGreeting()
-
-      try {
-        const result = await axios.put('/api/roles/' + this.role.id, {
-          name: this.role.name,
-          roles: this.role.roleItems,
-        })
-
-        if (result.status === 200) {
-          this.greeting = 'Update success'
-        }
-
-        this.initRoles()
-        this.closeEditRoleModal()
-      } catch (error) {
-        const status = error.response.status
-
-        if (status === 403) {
-          this.alert = 'Illegal operation was performed.'
-          this.closeEditRoleModal()
-        }
-
-        if (status === 422) {
-          var responseErrors = error.response.data.errors
-          var errors = {}
-          for (var key in responseErrors) {
-            errors[key] = responseErrors[key][0]
-          }
-          this.storeErrors = errors
-        }
-
-        if (status >= 500) {
-          this.alert = 'Server Error'
-          this.closeEditRoleModal()
-        }
       }
     },
 
@@ -516,12 +341,6 @@ export default {
       this.roles = result.data
     },
 
-    async initMenuItems() {
-      const result = await axios.get('api/menus/items')
-
-      this.menuItems = result.data
-    },
-
     async initRoleOperation() {
       this.finishInitialize = false
 
@@ -551,7 +370,6 @@ export default {
     async initialize() {
       this.initUsers()
       this.initRoles()
-      this.initMenuItems()
       this.initRoleOperation()
     },
   },
