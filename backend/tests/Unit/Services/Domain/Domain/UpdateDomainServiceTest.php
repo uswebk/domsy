ed1b177d5feba9d6505257ef0a1544f9c1f8fe16
uswebk <usweb111@gmail.com>
2022-10-12 00:13:15 +0900

    [test] update domain service test

---
+++ backend/tests/Unit/Services/Domain/Domain/UpdateDomainServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Infrastructures\Queries\User;
+
+use App\Infrastructures\Models\Domain;
+use App\Infrastructures\Models\Subdomain;
+use App\Infrastructures\Repositories\Domain\DomainRepository;
+use App\Infrastructures\Repositories\Subdomain\SubdomainRepository;
+use App\Services\Domain\Domain\UpdateDomainService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+final class UpdateDomainServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function if_domain_name_changes_recreate_subdomain(): void
+    {
+        $domain = Domain::factory([
+            'name' => 'test.com',
+        ])->create();
+
+        Subdomain::factory([
+            'domain_id' => $domain->id,
+        ])->count(10)->create();
+
+        $domain->name = 'test2.com';
+
+        $updateDomainService = new UpdateDomainService(new DomainRepository(), new SubdomainRepository());
+        $updateDomainService->execute($domain);
+
+        $this->assertDatabaseCount('subdomains', 1);
+        $this->assertDatabaseHas('domains', [
+            'name' => 'test2.com',
+        ]);
+    }
+
+    /** @test */
+    public function if_domain_name_not_changes_not_recreate_subdomain(): void
+    {
+        $domain = Domain::factory([
+            'name' => 'test.com',
+        ])->create();
+
+        Subdomain::factory([
+            'domain_id' => $domain->id,
+        ])->count(10)->create();
+
+        $updateDomainService = new UpdateDomainService(new DomainRepository(), new SubdomainRepository());
+        $updateDomainService->execute($domain);
+
+        $this->assertDatabaseCount('subdomains', 10);
+        $this->assertDatabaseHas('domains', [
+            'name' => 'test.com',
+        ]);
+    }
+}
