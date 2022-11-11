3cb1adef7de38f924ac192ad22d73fbb5d6e64cd
uswebk <usweb111@gmail.com>
2022-11-11 22:19:56 +0900

    [fix] changed password

--- backend/app/Http/Requests/Api/Account/UpdateRequest.php
+++ backend/app/Http/Requests/Api/Account/UpdateRequest.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Request;
 use App\Rules\Email;
 use App\Rules\RoleOwner;
 use App\Rules\RoleRequiredAdmin;
+use Illuminate\Support\Facades\Hash;
 
 final class UpdateRequest extends Request
 {
@@ -26,9 +27,20 @@ final class UpdateRequest extends Request
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => ['email','max:255',new Email($this->user)],
+            'email' => ['email', 'max:255', new Email($this->user)],
             'role_id' => [new RoleOwner(), new RoleRequiredAdmin($this->user)],
         ];
+    }
+
+    /**
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     */
+    public function withValidator(\Illuminate\Contracts\Validation\Validator $validator): void
+    {
+        $validator->sometimes('password', 'required|min:8|confirmed', function ($input) {
+            return array_key_exists('password', $input->toArray());
+        });
     }
 
     /**
@@ -36,10 +48,16 @@ final class UpdateRequest extends Request
      */
     public function makeInput(): array
     {
-        return [
+        $data = [
             'name' => $this->name,
             'email' => $this->email,
             'role_id' => $this->role_id,
         ];
+
+        if (isset($this->password)) {
+            $data = array_merge($data, ['password' => Hash::make($this->password),]);
+        }
+
+        return $data;
     }
 }
