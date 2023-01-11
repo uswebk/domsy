e39fff00fddcecd4346c9f0ef0118aafd0508ebf
uswebk <usweb111@gmail.com>
2023-01-11 23:23:09 +0900

    [test] add method getByUserIds to RegistrarQueryService

--- backend/tests/Unit/Queries/Registrar/EloquentRegistrarQueryServiceTest.php
+++ backend/tests/Unit/Queries/Registrar/EloquentRegistrarQueryServiceTest.php
@@ -63,4 +63,49 @@ final class EloquentRegistrarQueryServiceTest extends TestCase
             $this->assertTrue($assertException);
         }
     }
+
+    /**
+     * @return array
+     */
+    public function dataProviderOfGetByUserIds(): array
+    {
+        return [
+            'user_ids_match_domains' => [
+                [['id' => 1,], ['id' => 2,],],
+                [['id' => 1, 'user_id' => 1,], ['id' => 2, 'user_id' => 2,],],
+                [1, 2,],
+                2,
+            ],
+            'user_ids_not_match_domains' => [
+                [['id' => 1,], ['id' => 2,],],
+                [['id' => 1, 'user_id' => 1,], ['id' => 2, 'user_id' => 2,],],
+                [3, 4,],
+                0,
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProviderOfGetByUserIds
+     */
+    public function it_get_by_user_ids(
+        array $parameterOfUsers,
+        array $attrOfRegistrars,
+        array $userIds,
+        int $count
+    ): void {
+
+        foreach ($parameterOfUsers as $parameterOfUser) {
+            User::factory($parameterOfUser)->create();
+        }
+
+        foreach ($attrOfRegistrars as $attrOfRegistrar) {
+            Registrar::factory($attrOfRegistrar)->create();
+        }
+
+        $registrars = (new EloquentRegistrarQueryService())->getByUserIds($userIds);
+
+        $this->assertCount($count, $registrars);
+    }
 }
